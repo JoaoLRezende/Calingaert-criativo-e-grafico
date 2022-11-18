d6a00572fc7c1442d0005eed67c5be687f74f4ab
João Rezende <joaozinhobent@gmail.com>
2022-11-17 21:28:15 -0300

    Adicionar selecionador de arquivo de entrada

--- src/main/java/com/calingaertgrafico/calingaertlegalegrafico/HelloController.java
+++ src/main/java/com/calingaertgrafico/calingaertlegalegrafico/HelloController.java
@@ -7,7 +7,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloController {
@@ -65,5 +67,17 @@ public class HelloController {
     void onCarregarClick(ActionEvent event) throws IOException {
         HelloApplication.carregarPrograma(textField_arquivoEntrada.getText(), txt_outputConsole);
     }
+
+    @FXML
+    void carregarArquivo(ActionEvent event){
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arquivo Montado", "*.HPX"));
+        File f = fc.showOpenDialog(null);
+
+        if (f != null){
+            textField_arquivoEntrada.setText(f.getAbsolutePath());
+        }
+    }
+
 
 }
