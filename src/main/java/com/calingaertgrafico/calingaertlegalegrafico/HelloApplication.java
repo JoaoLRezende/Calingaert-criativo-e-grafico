0777cbd34b20f538ad6de5cec4eeef4c98c63e42
João Rezende <joaozinhobent@gmail.com>
2022-11-23 10:04:58 -0300

    Mudar nome da janela para "Calingaert 3000"

--- src/main/java/com/calingaertgrafico/calingaertlegalegrafico/HelloApplication.java
+++ src/main/java/com/calingaertgrafico/calingaertlegalegrafico/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 876, 542);
-        stage.setTitle("Hello!");
+        stage.setTitle("Calingaert 3000");
         stage.setScene(scene);
         stage.show();
 
