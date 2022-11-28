ab6fa0df82b91af3fedf671987ed6d8ab94399ff
João Ladeira Rezende <joaozinhobent@gmail.com>
2022-11-27 22:42:04 -0300

    Terminar de implementar Bubble Sort

--- src/main/java/com/calingaertgrafico/calingaertlegalegrafico/exemplos/bubble_sort/ordernar.asm
+++ src/main/java/com/calingaertgrafico/calingaertlegalegrafico/exemplos/bubble_sort/ordernar.asm
@@ -7,6 +7,7 @@
         MEND
 ordenar LOAD @121
         STORE curr_i
+        COPY swapped @0
 loop    LOAD curr_i
         ADD @1
         STORE curr_i
@@ -14,10 +15,19 @@ loop    LOAD curr_i
         STORE curr_i2
         LOAD &curr_i
         SUB &curr_i2
-        BRNEG loop
+        BRNEG end
         SWAP curr_i curr_i2
-        BR loop
+        LOAD swapped
+        ADD @1
+        STORE swapped
+* verificar se atingimos o final do array
+end     LOAD curr_i2
+        SUB @126
+        BRNEG loop
+        LOAD swapped
+        BRPOS ordenar
         RET
 tmp     SPACE   * usado pela macro SWAP
 curr_i  SPACE   * endereço do 1º elemento sendo comparado
 curr_i2 SPACE   * endereço do 2º elemento sendo comparado
+swapped CONST @0      * indica se houve algum swap nesta iteração
